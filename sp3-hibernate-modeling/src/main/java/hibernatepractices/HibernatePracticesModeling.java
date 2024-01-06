cea3a169ca4c635c58107fa0ad2ec8999d058a16
omid mohebbi <omidmohebbise@gmail.com>
2024-01-06 08:34:16 +0100

    SP3 Hibernate Modeling: Rename the main class

--- sp3-hibernate-modeling/src/main/java/hibernatepractices/HibernatePracticesModeling.java
+++ sp3-hibernate-modeling/src/main/java/hibernatepractices/HibernatePracticesModeling.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HibernatePracticesApplication {
+public class HibernatePracticesModeling {
 
     public static void main(String[] args) {
-        SpringApplication.run(HibernatePracticesApplication.class, args);
+        SpringApplication.run(HibernatePracticesModeling.class, args);
     }
 
 }
